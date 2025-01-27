4dd46231516db710e539addfad2bbde3c05b4d97
aimir <48388610+aimir@users.noreply.github.com>
2025-01-27 20:29:10 +0200

    Support address serialization and deserialization

--- ap2p/address.py
+++ ap2p/address.py
@@ -3,8 +3,10 @@ A module containing the Address class, which represents a network address.
 """
 
 from asyncio import open_connection, start_server
+from base64 import b64decode, b64encode
 from dataclasses import dataclass
 from ipaddress import ip_address
+from struct import pack, unpack
 
 
 @dataclass(frozen=True)
@@ -36,6 +38,38 @@ class Address:
             asyncio.base_events.Server: The server object.
         """
         return await start_server(callback, host=str(self.host), port=self.port)
+
+    @staticmethod
+    def from_bytes(addr_bytes: bytes):
+        """
+        Convert a 6-byte sequence into an Address instance.
+
+        Args:
+            addr_bytes (bytes): A 6-byte sequence representing the IP address
+                and port number, encoded in base64.
+
+        Returns:
+            Address: An Address instance created from the given bytes.
+        """
+        unwrapped = b64decode(addr_bytes)
+        if len(unwrapped) != 6:
+            raise ValueError("Address bytes must be exactly 6 bytes long")
+        host = ip_address(unwrapped[0:4])
+        port = unpack("!H", unwrapped[4:6])[0]
+        return Address(host=host, port=port)
+
+    def __bytes__(self):
+        """
+        Convert the Address instance to bytes for easy transmission over the network.
+
+        The returned bytes are in the following format:
+        - The first 4 bytes represent the packed IP address.
+        - The next 2 bytes represent the port number in network byte order.
+
+        Returns:
+            bytes: The byte representation of the Address instance.
+        """
+        return b64encode(self.host.packed + pack("!H", self.port))
 
     def __repr__(self):
         """
